88f3477eac46c194a2164050cc4dee279c265dde
0joEy <69210712+0joEy@users.noreply.github.com>
2023-08-18 09:56:18 -0600

    Fixed Sulfur Oxidization

--- src/main/java/net/henrycmoss/bb/item/custom/SulfurItem.java
+++ src/main/java/net/henrycmoss/bb/item/custom/SulfurItem.java
@@ -7,10 +7,13 @@ import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.LiquidBlock;
 import net.minecraft.world.level.material.Fluids;
 import net.minecraftforge.fluids.FluidType;
 
 public class SulfurItem extends Item {
+
+    private boolean continueSulfurOx = true;
 
     public SulfurItem(Properties pProperties) {
         super(pProperties);
@@ -18,11 +21,12 @@ public class SulfurItem extends Item {
 
     @Override
     public boolean onEntityItemUpdate(ItemStack stack, ItemEntity entity) {
-        if(entity.isInFluidType(Fluids.WATER.getFluidType())) {
+        if(entity.isInFluidType(Fluids.WATER.getFluidType()) && continueSulfurOx) {
             Level level = entity.level();
             level.setBlock(entity.blockPosition(), BbBlocks.ACID.get().defaultBlockState(), 3);
             LogUtils.getLogger().info("touched");
             entity.kill();
+            continueSulfurOx = false;
         }
 
         return super.onEntityItemUpdate(stack, entity);
